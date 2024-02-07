d1add901895dcaa5be0f48c1230e98ef83b36a3e
AzumattDev <azumattgaming@gmail.com>
2024-02-07 16:39:37 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace NoWeaponDust
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class NoWeaponDustPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "NoWeaponDust";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = $"{Author}.{ModName}";
+        private static string ConfigFileName = $"{ModGUID}.cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+        public static readonly ManualLogSource NoWeaponDustLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            RemoveTriggerEffects = config("1 - General", "RemoveTriggerEffects", Toggle.On, "If on, this will remove the dust effect from weapons (On Trigger). If off, it will not.");
+            RemoveHitTerrainEffects = config("1 - General", "RemoveHitTerrainEffects", Toggle.Off, "If on, this will remove the dust effect from weapons (On Hit Terrain). If off, it will not.");
+            RemoveHitEffects = config("1 - General", "RemoveHitEffects", Toggle.Off, "If on, this will remove the dust effect from weapons (On Hit). If off, it will not.");
+            RemoveStartEffects = config("1 - General", "RemoveStartEffects", Toggle.Off, "If on, this will remove the dust effect from weapons (On Start). If off, it will not.");
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                NoWeaponDustLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                NoWeaponDustLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                NoWeaponDustLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+        internal static void DisableEffectBasedOnConfig(EffectList effectList, Toggle shouldDisable)
+        {
+            if (effectList == null) return;
+
+            foreach (EffectList.EffectData? effect in effectList.m_effectPrefabs)
+            {
+                if(effect.m_prefab == null) continue;
+                if (effect.m_prefab.name.Contains("vfx_") || effect.m_prefab.name.StartsWith("fx_"))
+                {
+                    effect.m_enabled = shouldDisable == Toggle.Off;
+                }
+            }
+        }
+
+
+        #region ConfigOptions
+
+        internal static ConfigEntry<Toggle> RemoveTriggerEffects = null!;
+        internal static ConfigEntry<Toggle> RemoveHitTerrainEffects = null!;
+        internal static ConfigEntry<Toggle> RemoveHitEffects = null!;
+        internal static ConfigEntry<Toggle> RemoveStartEffects = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description)
+        {
+            return config(group, name, value, new ConfigDescription(description));
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            [UsedImplicitly] public int? Order = null!;
+            [UsedImplicitly] public bool? Browsable = null!;
+            [UsedImplicitly] public string? Category = null!;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer = null!;
+        }
+
+        #endregion
+    }
+
+    [HarmonyPatch(typeof(Attack), nameof(Attack.DoAreaAttack))]
+    static class AttackDoAreaAttackPatch
+    {
+        static void Prefix(Attack __instance)
+        {
+            if (!__instance.m_character.IsPlayer()) return;
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_hitTerrainEffect, NoWeaponDustPlugin.RemoveHitTerrainEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_weapon.m_shared.m_hitTerrainEffect, NoWeaponDustPlugin.RemoveHitTerrainEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_hitEffect, NoWeaponDustPlugin.RemoveHitEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_weapon.m_shared.m_hitEffect, NoWeaponDustPlugin.RemoveHitEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_triggerEffect, NoWeaponDustPlugin.RemoveTriggerEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_weapon.m_shared.m_triggerEffect, NoWeaponDustPlugin.RemoveTriggerEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_startEffect, NoWeaponDustPlugin.RemoveStartEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_weapon.m_shared.m_startEffect, NoWeaponDustPlugin.RemoveStartEffects.Value);
+        }
+    }
+
+    [HarmonyPatch(typeof(Attack), nameof(Attack.DoNonAttack))]
+    static class AttackDoNonAttackPatch
+    {
+        static void Prefix(Attack __instance)
+        {
+            if (!__instance.m_character.IsPlayer()) return;
+
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_triggerEffect, NoWeaponDustPlugin.RemoveTriggerEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_weapon.m_shared.m_triggerEffect, NoWeaponDustPlugin.RemoveTriggerEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_startEffect, NoWeaponDustPlugin.RemoveStartEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_weapon.m_shared.m_startEffect, NoWeaponDustPlugin.RemoveStartEffects.Value);
+        }
+    }
+
+    [HarmonyPatch(typeof(Attack), nameof(Attack.DoMeleeAttack))]
+    static class AttackOnAttackTriggerPatch
+    {
+        static void Prefix(Attack __instance)
+        {
+            if (!__instance.m_character.IsPlayer()) return;
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_hitTerrainEffect, NoWeaponDustPlugin.RemoveHitTerrainEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_weapon.m_shared.m_hitTerrainEffect, NoWeaponDustPlugin.RemoveHitTerrainEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_hitEffect, NoWeaponDustPlugin.RemoveHitEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_weapon.m_shared.m_hitEffect, NoWeaponDustPlugin.RemoveHitEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_triggerEffect, NoWeaponDustPlugin.RemoveTriggerEffects.Value);
+            NoWeaponDustPlugin.DisableEffectBasedOnConfig(__instance.m_weapon.m_shared.m_triggerEffect, NoWeaponDustPlugin.RemoveTriggerEffects.Value);
+        }
+    }
+}
---
+++ Thunderstore/README.md
@@ -0,0 +1,72 @@
+# Description
+
+## NoWeaponDust is a mod for Valheim that aims to enhance the player's experience by providing options to remove the visual effects (VFX) seen using your weapon when attacking. Specifically, by default it disables the default "dust" effects from weapon attack triggers, with further options to remove additional effects at different points in the attack cycle.
+
+`Client Only Mod`
+
+---
+
+## Features
+
+- **RemoveTriggerEffects**: Enables removal of dust effects triggered by weapon attacks, providing a cleaner combat visual experience. Most noticeable when using Sledges.
+- **RemoveHitTerrainEffects**: Allows for the disabling of dust effects when weapons hit terrain, keeping the battlefield less cluttered.
+- **RemoveHitEffects**: Offers the option to remove dust effects upon hitting, focusing on the impact without additional VFX distractions.
+- **RemoveStartEffects**: Permits the elimination of initial attack effects, streamlining the visual feedback of combat actions.
+
+## Compatibility
+- Designed to be compatible with most other mods. However, mods that significantly alter creature death mechanics may conflict (not tested with many mods). Though should be more compatible than the other alternative instant loot mods.
+
+<details>
+<summary><b>Installation Instructions</b></summary>
+
+***You must have BepInEx installed correctly! I can not stress this enough.***
+
+### Manual Installation
+
+`Note: (Manual installation is likely how you have to do this on a server, make sure BepInEx is installed on the server correctly)`
+
+1. **Download the latest release of BepInEx.**
+2. **Extract the contents of the zip file to your game's root folder.**
+3. **Download the latest release of NoWeaponDust from Thunderstore.io.**
+4. **Extract the contents of the zip file to the `BepInEx/plugins` folder.**
+5. **Launch the game.**
+
+### Installation through r2modman or Thunderstore Mod Manager
+
+1. **Install [r2modman](https://valheim.thunderstore.io/package/ebkr/r2modman/)
+   or [Thunderstore Mod Manager](https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager).**
+
+   > For r2modman, you can also install it through the Thunderstore site.
+   ![](https://i.imgur.com/s4X4rEs.png "r2modman Download")
+
+   > For Thunderstore Mod Manager, you can also install it through the Overwolf app store
+   ![](https://i.imgur.com/HQLZFp4.png "Thunderstore Mod Manager Download")
+2. **Open the Mod Manager and search for "NoWeaponDust" under the Online
+   tab. `Note: You can also search for "Azumatt" to find all my mods.`**
+
+   `The image below shows VikingShip as an example, but it was easier to reuse the image.`
+
+   ![](https://i.imgur.com/5CR5XKu.png)
+
+3. **Click the Download button to install the mod.**
+4. **Launch the game.**
+
+</details>
+
+<br>
+<br>
+
+`Feel free to reach out to me on discord if you need manual download assistance.`
+
+# Author Information
+
+### Azumatt
+
+`DISCORD:` Azumatt#2625
+
+`STEAM:` https://steamcommunity.com/id/azumatt/
+
+For Questions or Comments, find me in the Odin Plus Team Discord or in mine:
+
+[![https://i.imgur.com/XXP6HCU.png](https://i.imgur.com/XXP6HCU.png)](https://discord.gg/qhr2dWNEYq)
+<a href="https://discord.gg/pdHgy6Bsng"><img src="https://i.imgur.com/Xlcbmm9.png" href="https://discord.gg/pdHgy6Bsng" width="175" height="175"></a>
